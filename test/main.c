53b78b20b700cba50671ad23cbf16d7151b6d598
Samuel YEVI <samuelyevidev777@gmail.com>
2026-08-10 00:33:35 +0100

    Add multilinear test suite and register it in the test runner

--- test/main.c
+++ test/main.c
@@ -14,6 +14,7 @@
 #include <cmocka.h>
 
 #include "test_complex.h"
+#include "test_multilinear.h"
 #include "test_status.h"
 
 
@@ -24,6 +25,7 @@ main(void)
 
     failures += numc_status_tests_run();
     failures += numc_complex_tests_run();
+    failures += numc_multilinear_tests_run();
 
     /* Ajoute ici tes prochains groupes de tests, ex. :
      * failures += numc_lin_alg_tests_run();
